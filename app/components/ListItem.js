d0085a1c874a5ebba0e44940e0ab4029d11b84f3
Aaron Ledbetter <aaronledbetter@Aarons-MacBook-Air.local>
2022-11-19 15:14:10 -0700

    app now has swipe functions, refresh, and delete capabilities, message screen is completed.

--- app/components/ListItem.js
+++ app/components/ListItem.js
@@ -1,17 +1,24 @@
 import React from 'react';
-import { View, StyleSheet, Image } from 'react-native';
+import { View, StyleSheet, Image, TouchableHighlight } from 'react-native';
 import colors from '../config/colors';
 import AppText from './AppText';
+import { Swipeable } from 'react-native-gesture-handler';
 
-function ListItem({title, subTitle, image}) {
+function ListItem({title, subTitle, image, onPress, renderRightActions}) {
     return (
+        <Swipeable renderRightActions={renderRightActions}>
+       <TouchableHighlight 
+       underlayColor={colors.light}
+       onPress= {onPress}>
         <View style={styles.container}>
-            <Image style={styles.image} source={image} />
-            <View>
-                <AppText style={styles.title}>{title}</AppText>
-                <AppText style={styles.subTitle}>{subTitle}</AppText>
+                <Image style={styles.image} source={image} />
+                <View>
+                    <AppText style={styles.title}>{title}</AppText>
+                    <AppText style={styles.subTitle}>{subTitle}</AppText>
+                </View>
             </View>
-        </View>
+        </TouchableHighlight>
+        </Swipeable>
     );
 }
 
--- app/screens/MessagesScreen.js
+++ app/screens/MessagesScreen.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { FlatList, StyleSheet, View } from 'react-native';
 import ListItem from '../components/ListItem';
 import Screen from '../components/Screen';
 import ListItemSeperator from '../components/ListItemSeperator'
+import ListItemDeleteAction from '../components/ListItemDeleteAction';
 
-const messages = [
+const initialMessages = [
     {
         id: 1,
         title: 'T1',
@@ -20,6 +21,13 @@ const messages = [
 ]
 
 function MessagesScreen(props) {
+    const [messages, setMessages] = useState(initialMessages);
+    const [refreshing, setRefreshing] = useState(false);
+
+    const handleDelete = message => {
+       setMessages(messages.filter((m) => m.id !== message.id));
+    }   
+
     return (
         <Screen>
             <FlatList
@@ -30,9 +38,23 @@ function MessagesScreen(props) {
                     title={item.title}
                     subTitle={item.description}
                     image={item.image}
+                    onPress={() => console.log("Message Selected", item)}
+                    renderRightActions={() => 
+                    <ListItemDeleteAction onPress={() => handleDelete(item)}/>}
                 /> 
             )} 
             ItemSeparatorComponent={ListItemSeperator}
+            refreshing={refreshing}
+            onRefresh={() => {
+                setMessages([
+                    {
+                        id: 2,
+                        title: 'T2',
+                        description: 'D2',
+                        image: {uri: "https://picsum.photos/200" }
+                    }
+                ])
+            }}
             />
         </Screen>
     );

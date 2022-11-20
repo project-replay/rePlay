7a7f09a60c2d6605c302a93c90598faced9e0831
Aaron Ledbetter <aaronledbetter@Aarons-MacBook-Air.local>
2022-11-20 12:23:30 -0700

    changed button comp

--- app/components/AppButton.js
+++ app/components/AppButton.js
@@ -3,51 +3,29 @@ import { StyleSheet, Text, TouchableOpacity } from 'react-native';
 import AppText from './AppText';
 import colors from '../config/colors';
 
-function AppButton({
-	component, // Namely for images and vector icons
-	title,
-	onPress,
-	width = '80%',
-	bgColor,
-	textColor,
-	fontSize,
-	hasBorder = false,
-	borderRadius = 30,
-}) {
-	return (
-		<TouchableOpacity
-			style={[
-				styles.button,
-				{ width: width },
-				{ backgroundColor: colors[bgColor] },
-				hasBorder && styles.border,
-				{ borderRadius: borderRadius },
-			]}
-			onPress={onPress}>
-			{component}
-			<AppText text={title} fontSize={fontSize} textColor={textColor} />
-		</TouchableOpacity>
-	);
+function AppButton({title, onPress, color = "primary"}) {
+    return (
+        <TouchableOpacity style={[styles.button, { backgroundColor: colors[color]}]} onPress={onPress}>
+            <Text style={styles.text}>{title}</Text>
+        </TouchableOpacity>
+    );
 }
 
 const styles = StyleSheet.create({
 	button: {
-		display: 'flex',
-		flexDirection: 'row',
-		flexWrap: 'nowrap',
-		justifyContent: 'center',
-		alignItems: 'center',
-		width: '80%',
-		margin: 10,
-		padding: 12,
-		borderRadius: 30,
-		backgroundColor: colors.primary,
-	},
-	border: {
-		borderColor: colors.primary,
-		borderStyle: 'solid',
-		borderWidth: 1,
-	},
+        backgroundColor: colors.primary,
+        borderRadius: 25,
+        justifyContent: "center",
+        alignItems: "center",
+        padding: 5,
+        width: "100%",
+    },
+    text: {
+        color: colors.white,
+        fontSize: 18,
+        textTransform: "uppercase",
+        fontWeight: "bold",
+    },
 });
 
 export default AppButton;

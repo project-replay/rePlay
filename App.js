91daea0f05e7e25d0340f1f30ebd3353ed5d4832
jyaymie <104411072+jyaymie@users.noreply.github.com>
2022-11-20 20:47:13 -0800

    Obtain imageUrl from cloud storage

--- App.js
+++ App.js
@@ -1,38 +1,43 @@
 import React, { useState } from 'react';
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, SafeAreaView, View, Switch } from 'react-native';
-import ViewImageScreen from './app/screens/ViewImageScreen';
-import WelcomeScreen from './app/screens/WelcomeScreen';
-import AppText from './app/components/AppText';
+import { StatusBar } from 'expo-status-bar';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 import colors from './app/config/colors';
+
+// Components
+import AppText from './app/components/AppText';
 import AppButton from './app/components/AppButton';
 import Card from './app/components/Card';
-import ListingDetailsScreen from './app/screens/ListingDetailsScreen';
-import MessagesScreen from './app/screens/MessagesScreen';
 import Camera from './app/components/Camera';
 import Screen from './app/components/Screen';
 import Icon from './app/components/Icon';
 import ListItem from './app/components/ListItem';
+import AppPicker from './app/components/AppPicker';
+import AppTextInput from './app/components/AppTextInput';
+
+// Screen
+import ViewImageScreen from './app/screens/ViewImageScreen';
+import WelcomeScreen from './app/screens/WelcomeScreen';
+import ListingDetailsScreen from './app/screens/ListingDetailsScreen';
+import MessagesScreen from './app/screens/MessagesScreen';
 import MyAccountScreen from './app/screens/AccountScreen';
 import AccountScreen from './app/screens/AccountScreen';
 import ListingsScreen from './app/screens/ListingsScreen';
-import AppTextInput from './app/components/AppTextInput';
-import AppPicker from './app/components/AppPicker';
 import LoginScreen from './app/screens/LoginScreen';
 import RegisterScreen from './app/screens/RegisterFormScreen';
 import EditScreen from './app/screens/EditListing';
 
-export default function App() {
+export const AppStateContext = React.createContext();
 
-  return <EditScreen />
+function App() {
+	// Globally set the url when the image is successfully stored in the cloud
+	const [imageUrl, setImageUrl] = useState('');
+
+	return (
+		<AppStateContext.Provider value={{ imageUrl, setImageUrl }}>
+			<Camera />
+		</AppStateContext.Provider>
+	);
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    colors,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;

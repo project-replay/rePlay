d85f885e5f8a4c695a7f023a4a9e8ea55c08ecec
Aaron Ledbetter <aaronledbetter@Aarons-MacBook-Air.local>
2022-11-21 05:39:50 -0700

    set nav for registration

--- app/navigation/AuthNavigator.js
+++ app/navigation/AuthNavigator.js
@@ -4,6 +4,7 @@ import {createStackNavigator } from '@react-navigation/stack';
 
 import LoginScreen from '../screens/LoginScreen';
 import WelcomeScreen from '../screens/WelcomeScreen';
+import RegisterFormScreen from '../screens/RegisterFormScreen';
 
 const Stack = createStackNavigator();
 
@@ -11,6 +12,7 @@ const AuthNavigator = () => (
     <Stack.Navigator>
         <Stack.Screen name="Welcome" component={WelcomeScreen} options={{headerShown: false}} />
         <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Register" component={RegisterFormScreen} />
 
     </Stack.Navigator>
 );

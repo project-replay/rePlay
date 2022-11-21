19f2ec7b63bf25de436d8a0ef84e918555e4fa6a
jyaymie <104411072+jyaymie@users.noreply.github.com>
2022-11-20 17:50:41 -0800

    Add images && correct typos

--- app/screens/AccountScreen.js
+++ app/screens/AccountScreen.js
@@ -4,71 +4,68 @@ import Screen from '../components/Screen';
 import Icon from '../components/Icon';
 import { FlatList, StyleSheet, View } from 'react-native';
 import colors from '../config/colors';
-import ListItemSeperator from '../components/ListItemSeperator';
+import ListItemSeparator from '../components/ListItemSeparator';
 
 const menuItems = [
-    {
-        title: "My Listings",
-        icon: {
-            name: "format-list-bulleted",
-            backgroundColor: colors.primary,
-        }
-    },
-    {
-        title: "My Messages",
-        icon: {
-            name: "email",
-            backgroundColor: colors.secondary,
-        }
-    }
-]
+	{
+		title: 'My Listings',
+		icon: {
+			name: 'format-list-bulleted',
+			backgroundColor: colors.primary,
+		},
+	},
+	{
+		title: 'My Messages',
+		icon: {
+			name: 'email',
+			backgroundColor: colors.secondary,
+		},
+	},
+];
 
 function AccountScreen(props) {
-    return (
-        <Screen style={styles.screen}>
-            <View style={styles.continer}>
-                <ListItem 
-                title="My Name"
-                subTitle="youremailhere@gmail.com"
-                image={{uri:"https://picsum.photos/200"}}
-                />
-            </View>
-            <View style={styles.continer}>
-                <FlatList
-                    data={menuItems}
-                    keyExtractor={menuItem => menuItem.title}
-                    ItemSeparatorComponent={ListItemSeperator}
-                    renderItem={({ item }) => 
-                        <ListItem
-                        title={item.title}
-                        IconComponent={
-                            <Icon name={item.icon.name}
-                            backgroundColor={item.icon.backgroundColor}
-                            />
-                        }
-                        />
-                } 
-            />
-            </View>
-            <ListItem
-            title="Log Out"
-            IconComponent={
-                <Icon name="logout"
-                backgroundColor='#ffe66d'
-                />
-            }
-            />
-        </Screen>
-    );
+	return (
+		<Screen style={styles.screen}>
+			<View style={styles.container}>
+				<ListItem
+					title='My Name'
+					subTitle='youremailhere@gmail.com'
+					image={{ uri: 'https://picsum.photos/200' }}
+				/>
+			</View>
+			<View style={styles.container}>
+				<FlatList
+					data={menuItems}
+					keyExtractor={(menuItem) => menuItem.title}
+					ItemSeparatorComponent={ListItemSeparator}
+					renderItem={({ item }) => (
+						<ListItem
+							title={item.title}
+							IconComponent={
+								<Icon
+									name={item.icon.name}
+									backgroundColor={item.icon.backgroundColor}
+								/>
+							}
+						/>
+					)}
+				/>
+			</View>
+			<ListItem
+				title='Log Out'
+				IconComponent={<Icon name='logout' backgroundColor='#ffe66d' />}
+			/>
+		</Screen>
+	);
 }
 
 const styles = StyleSheet.create({
-    continer: {
-        marginVertical: 20,
-    },
-    screen: {
-        backgroundColor: colors.light
-    }
-})
+	container: {
+		marginVertical: 20,
+	},
+	screen: {
+		backgroundColor: colors.light,
+	},
+});
 
 export default AccountScreen;
--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -1,89 +1,85 @@
 import React from 'react';
-import {Image, StyleSheet, Text, View} from 'react-native'
+import { Image, StyleSheet, Text, View } from 'react-native';
 import AppButton from '../components/AppButton';
 import AppTextInput from '../components/AppTextInput';
 import Screen from '../components/Screen';
 import ReplayLogo from '../assets/svg/ReplayLogo';
 import colors from '../config/colors';
-import Train from '../assets/svg/Train'
-import ListItemSeperator from '../components/ListItemSeperator';
-import {Formik} from 'formik';
+import Train from '../assets/svg/Train';
+import ListItemSeparator from '../components/ListItemSeparator';
+import { Formik } from 'formik';
 
 function LoginScreen(props) {
+	return (
+		<Screen style={styles.container}>
+			<View style={styles.header}>
+				<ReplayLogo />
+			</View>
+			<ListItemSeparator />
+			<View style={styles.train}>
+				<Train />
+			</View>
 
-    return (
-        <Screen style={styles.container}>
-            <View style={styles.header}>
-            <ReplayLogo  />
-            </View>
-            <ListItemSeperator />
-            <View style={styles.train}>
-                <Train />
-            </View>
-
-            <Formik
-            initialValues={{email: "", password: ""}}
-            onSubmit={values => console.log(values)}
-            >
-                
-                {({ handleChange, handleSubmit}) => (
-                <>
-                    <AppTextInput
-            autoCapitalize="none"
-            autoCorrect={false}
-            icon="email"
-            keyboardType="email-address"
-            onChangeText={handleChange("email")}
-            placeholder="Email"
-            textContentType="emailAddress"
-            />
-            <AppTextInput 
-            autoCapitalize="none"
-            autoCorrect={false}
-            icon="lock"
-            onChangeText={handleChange("password")}
-            placeholder="Password"
-            secureTextEntry
-            textContentType="password"
-            />
-            <AppButton style={styles.text}
-            title="Login"
-            onPress={handleSubmit}
-            bgColor= "primary"
-            />
-                </>
-                )}
-                
-            </Formik>
-            
-        </Screen>
-    );
+			<Formik
+				initialValues={{ email: '', password: '' }}
+				onSubmit={(values) => console.log(values)}>
+				{({ handleChange, handleSubmit }) => (
+					<>
+						<AppTextInput
+							autoCapitalize='none'
+							autoCorrect={false}
+							icon='email'
+							keyboardType='email-address'
+							onChangeText={handleChange('email')}
+							placeholder='Email'
+							textContentType='emailAddress'
+						/>
+						<AppTextInput
+							autoCapitalize='none'
+							autoCorrect={false}
+							icon='lock'
+							onChangeText={handleChange('password')}
+							placeholder='Password'
+							secureTextEntry
+							textContentType='password'
+						/>
+						<AppButton
+							style={styles.text}
+							title='Login'
+							onPress={handleSubmit}
+							bgColor='primary'
+						/>
+					</>
+				)}
+			</Formik>
+		</Screen>
+	);
 }
 const styles = StyleSheet.create({
-    container: {
-        padding: 10,
-        backgroundColor: colors.light
-    },
-    header: {
-        justifyContent: "center",
-        alignSelf: "center",
-        margin: 0,
-    },
-    logo: {
-        width: 80,
-        height: 80,
-        alignSelf: "center",
-        marginTop: 50,
-        marginBottom: 20,
-    },
-    text: {
-        color: "white"
-    },
-    train: {
-        justifyContent: "center",
-        alignItems: "center",
-        margin: 10,
-    }
-})
+	container: {
+		padding: 10,
+		backgroundColor: colors.light,
+	},
+	header: {
+		justifyContent: 'center',
+		alignSelf: 'center',
+		margin: 0,
+	},
+	logo: {
+		width: 80,
+		height: 80,
+		alignSelf: 'center',
+		marginTop: 50,
+		marginBottom: 20,
+	},
+	text: {
+		color: 'white',
+	},
+	train: {
+		justifyContent: 'center',
+		alignItems: 'center',
+		margin: 10,
+	},
+});
 
 export default LoginScreen;

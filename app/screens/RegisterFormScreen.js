ae81d89393eb260c94a978d8009f21e8ad2a43b2
jyaymie <104411072+jyaymie@users.noreply.github.com>
2022-11-30 21:49:53 -0800

    Correct register function

--- app/screens/RegisterFormScreen.js
+++ app/screens/RegisterFormScreen.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import {
 	Image,
 	KeyboardAvoidingView,
@@ -8,7 +8,6 @@ import {
 	TextInput,
 	View,
 } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
 import { auth } from '../../firebase';
 import { createUserWithEmailAndPassword } from 'firebase/auth';
 import Checkbox from 'expo-checkbox';
@@ -16,12 +15,11 @@ import colors from '../config/colors';
 
 // Components and Assets
 import AppButton from '../components/AppButton';
+import Crayons from '../assets/png/crayons.png';
 import PageHeaderNoBell from '../components/PageHeaderNoBell';
 import Screen from '../components/Screen';
-import Crayons from '../assets/png/crayons.png';
 
-const RegisterFormScreen = () => {
-	const navigation = useNavigation();
+const RegisterFormScreen = ({ navigation }) => {
 	const [firstName, setFirstName] = useState('');
 	const [lastName, setLastName] = useState('');
 	const [username, setUsername] = useState('');
@@ -35,19 +33,6 @@ const RegisterFormScreen = () => {
 	const [state, setState] = useState('');
 	const [hasAgreed, setHasAgreed] = useState(false);
 
-	// If user is logged in, navigate them to Home
-	useEffect(() => {
-		const unsubscribe = auth.onAuthStateChanged((user) => {
-			if (user) {
-				navigation.navigate('WelcomeScreen');
-			}
-		});
-
-		return () => {
-			unsubscribe();
-		};
-	}, []);
-
 	// Register user
 	const handleRegister = async () => {
 		try {
@@ -56,125 +41,127 @@ const RegisterFormScreen = () => {
 				email,
 				password
 			);
-			// const user = userCredentials.user;
+			if (userCredentials) {
+				navigation.navigate('Feed');
+			}
 		} catch (error) {
-			alert(error.message);
+			alert(`Something went wrong.\nPlease try again.`);
 		}
 	};
 
 	return (
-		// <Screen>
-		<ScrollView>
-		<KeyboardAvoidingView
-			style={styles.container}
-			behavior='padding' // Prevents keyboard from covering input fields
-		>
-			<PageHeaderNoBell />
-			<View style={styles.imageContainer}>
-				<Image source={Crayons} style={styles.image} />
-			</View>
-			<Text style={styles.title}>Create an Account</Text>
-			<View style={styles.inputContainer}>
-				<TextInput
-					style={styles.input}
-					placeholder='First Name'
-					value={firstName}
-					onChangeText={(text) => setFirstName(text)}
-				/>
-				<TextInput
-					style={styles.input}
-					placeholder='Last Name'
-					value={lastName}
-					onChangeText={(text) => setLastName(text)}
-				/>
-				<TextInput
-					style={styles.input}
-					placeholder='Username'
-					autoCapitalize={false}
-					value={username}
-					onChangeText={(text) => setUsername(text)}
-				/>
-				<TextInput
-					style={styles.input}
-					placeholder='Email'
-					autoCapitalize={false}
-					value={email}
-					onChangeText={(text) => setEmail(text)}
-				/>
-				<TextInput
-					style={styles.input}
-					placeholder='Password'
-					secureTextEntry={true} // Shows password text as dots
-					value={password}
-					onChangeText={(text) => setPassword(text)}
-				/>
-				<TextInput
-					style={styles.input}
-					placeholder='Confirm Password'
-					secureTextEntry={true}
-					value={confirmPassword}
-					onChangeText={(text) => setConfirmPassword(text)}
-				/>
-				<TextInput
-					style={styles.input}
-					placeholder='Address line 1'
-					value={address1}
-					onChangeText={(text) => setAddress1(text)}
-				/>
-				<TextInput
-					style={styles.input}
-					placeholder='Address line 2'
-					value={address2}
-					onChangeText={(text) => setAddress2(text)}
-				/>
-				<TextInput
-					style={styles.input}
-					placeholder='City'
-					value={city}
-					onChangeText={(text) => setCity(text)}
-				/>
-				<TextInput
-					style={styles.input}
-					placeholder='State'
-					value={state}
-					onChangeText={(text) => setState(text)}
-				/>
-				<TextInput
-					style={styles.input}
-					placeholder='Zip Code'
-					value={zipCode}
-					onChangeText={(text) => setZipCode(text)}
-				/>
-				<View style={styles.checkboxContainer}>
-					<Checkbox
-						style={styles.checkbox}
-						value={hasAgreed}
-						onValueChange={(text) => setHasAgreed(text)}
-					/>
-					<Text style={styles.checkboxText}>
-						I accept the{' '}
-						<Text style={styles.legalDoc} onPress={() => {}}>
-							Terms & Conditions
-						</Text>
-					</Text>
-				</View>
-			</View>
-			<View style={styles.buttonContainer}>
-				<AppButton
-					title='Create Your Account'
-					onPress={() => {}}
-					style={styles.submitButton}
-				/>
-				<AppButton
-					title='Cancel'
-					onPress={() => {}}
-					bgColor='light'
-					style={styles.cancelButton}
-				/>
-			</View>
-		</KeyboardAvoidingView>
-		</ScrollView>
-		// </Screen>
+		<Screen>
+			<ScrollView>
+				<KeyboardAvoidingView
+					style={styles.container}
+					behavior='padding' // Prevents keyboard from covering input fields
+				>
+					<PageHeaderNoBell />
+					<View style={styles.imageContainer}>
+						<Image source={Crayons} style={styles.image} />
+					</View>
+					<Text style={styles.title}>Create an Account</Text>
+					<View style={styles.inputContainer}>
+						<TextInput
+							style={styles.input}
+							placeholder='First Name'
+							value={firstName}
+							onChangeText={(text) => setFirstName(text)}
+						/>
+						<TextInput
+							style={styles.input}
+							placeholder='Last Name'
+							value={lastName}
+							onChangeText={(text) => setLastName(text)}
+						/>
+						<TextInput
+							style={styles.input}
+							placeholder='Username'
+							autoCapitalize={false}
+							value={username}
+							onChangeText={(text) => setUsername(text)}
+						/>
+						<TextInput
+							style={styles.input}
+							placeholder='Email'
+							autoCapitalize={false}
+							value={email}
+							onChangeText={(text) => setEmail(text)}
+						/>
+						<TextInput
+							style={styles.input}
+							placeholder='Password'
+							secureTextEntry={true} // Shows password text as dots
+							value={password}
+							onChangeText={(text) => setPassword(text)}
+						/>
+						<TextInput
+							style={styles.input}
+							placeholder='Confirm Password'
+							secureTextEntry={true}
+							value={confirmPassword}
+							onChangeText={(text) => setConfirmPassword(text)}
+						/>
+						<TextInput
+							style={styles.input}
+							placeholder='Address line 1'
+							value={address1}
+							onChangeText={(text) => setAddress1(text)}
+						/>
+						<TextInput
+							style={styles.input}
+							placeholder='Address line 2'
+							value={address2}
+							onChangeText={(text) => setAddress2(text)}
+						/>
+						<TextInput
+							style={styles.input}
+							placeholder='City'
+							value={city}
+							onChangeText={(text) => setCity(text)}
+						/>
+						<TextInput
+							style={styles.input}
+							placeholder='State'
+							value={state}
+							onChangeText={(text) => setState(text)}
+						/>
+						<TextInput
+							style={styles.input}
+							placeholder='Zip Code'
+							value={zipCode}
+							onChangeText={(text) => setZipCode(text)}
+						/>
+						<View style={styles.checkboxContainer}>
+							<Checkbox
+								style={styles.checkbox}
+								value={hasAgreed}
+								onValueChange={(text) => setHasAgreed(text)}
+							/>
+							<Text style={styles.checkboxText}>
+								I accept the{' '}
+								<Text style={styles.legalDoc} onPress={() => {}}>
+									Terms & Conditions
+								</Text>
+							</Text>
+						</View>
+					</View>
+					<View style={styles.buttonContainer}>
+						<AppButton
+							title='Create Your Account'
+							onPress={handleRegister}
+							style={styles.submitButton}
+						/>
+						<AppButton
+							title='Cancel'
+							onPress={() => {}}
+							bgColor='light'
+							style={styles.cancelButton}
+						/>
+					</View>
+				</KeyboardAvoidingView>
+			</ScrollView>
+		</Screen>
 	);
 };
 
